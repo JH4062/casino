3d0c8364157e5e32caa1c4eb8db22505af696fa6
박장현 <p5725@cau.ac.kr>
2020-06-01 23:10:45 +0900

    과일을 슬롯 ABC로 나눴음. 매크로로 과일을 지정함. 배경음악 및 효과음 추가함. 주석 수정했음.

--- casino.cpp
+++ casino.cpp
@@ -12,9 +12,8 @@
 1. 키보드 입력을 스페이스로 교체
 2. money를 모든 씬과 연동해야함
 3. 이미지 필요: 슬롯 머신 뒷 배경/ 가격표
-4. 배경음악 및 효과음 삽입하기
-5. 필요하다면? 애니메이션 넣기
-6. 필요하다면? 카지노 게임 더 추가하기 (ex. 주사위 게임 )
+4. 배경음악이 끝날때 callback으로 다시 실행시키고 싶은데 방법을 모르겠다
+5. 과일별 출현 확률 수정
 =========================================================================*/
 
 													
@@ -23,31 +22,46 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define APPLE 0
+#define GRAPE 1 
+#define ORANGE 2
+
 #define ANIMATION_TIME		2
 
-ObjectID fruit[9];
+ObjectID fruitA[3], fruitB[3], fruitC[3];
+int num1, num2, num3;
+
 SceneID scene1;
 ObjectID machine;
 
 TimerID timer;
 
-int num1, num2, num3;
+SoundID bgm, coin, win;
 
-
-void Gameinit() {
+void Gameinit() { 
 
 	scene1 = createScene("바탕화면", "고쳐야 됨!.png"); // 카지노 바탕화면 필요할듯
 	machine = createObject("machine.png");
 	locateObject(machine, scene1, 350, 250);
 	showObject(machine);
 
-	//object를 배열화
-	char fruitImage[30];
-	for (int i = 0; i < 9; i++) {
-		sprintf_s(fruitImage, "fruit%d.png", i);
-		fruit[i] = createObject(fruitImage);
+	//슬롯머신에 사용되는 과일들 생성 후 배열에 넣어 사용
+	char fruitImage[20];
+	for (int i = 0; i < 3; i++) {
+		sprintf_s(fruitImage, "fruitA%d.png", i);
+		sprintf_s(fruitImage, "fruitB%d.png", i);
+		sprintf_s(fruitImage, "fruitC%d.png", i);
+		fruitA[i] = createObject(fruitImage);
+		fruitB[i] = createObject(fruitImage);
+		fruitC[i] = createObject(fruitImage);
 	}
 
+	bgm = createSound("casinoBackground.wav");
+	playSound(bgm);
+
+	coin = createSound("coinEffect.wav");
+
+	win = createSound("win.mp3");
 }
 
 void timerCallback(TimerID timer)
@@ -56,59 +70,65 @@ void timerCallback(TimerID timer)
 	startTimer(timer);
 }
 
-
-int slot1() {
-	
+int slotA() {  //slotB에 무작위 과일에 해당하는 난수를 생성한다
 	num1 = rand() % 3;
 	return num1;
 }
 
-int slot2() {
-
-	num2 = rand() % 3 + 3;
+int slotB() { //slotB에 무작위 과일에 해당하는 난수를 생성한다
+	num2 = rand() % 3;
 	return num2;
 }
 
-int slot3() {
-
-	num3 = rand() % 3 + 6;
+int slotC() { //slotB에 무작위 과일에 해당하는 난수를 생성한다
+	num3 = rand() % 3;
 	return num3;
 }
 
-
 void Roulette() {
-
-	for (int i = 0; i < 9; i++) { // 카지노 슬롯 초기화
-		hideObject(fruit[i]);
+	for (int i = 0; i < 3; i++) { // 화면에 보이는 카지노 슬롯 초기화
+		hideObject(fruitA[i]);
+		hideObject(fruitB[i]);
+		hideObject(fruitC[i]);
 	}
 
 	int xLocate[3] = { 380, 540, 700 };
 
-	locateObject(fruit[slot1()], scene1, xLocate[0], 300); // slot1
-	showObject(fruit[num1]);
+	locateObject(fruitA[slotA()], scene1, xLocate[0], 300); // A 슬롯에 무작위 과일 이미지를 화면에 출력한다
+	showObject(fruitA[num1]);
 	
-	locateObject(fruit[slot2()], scene1, xLocate[1], 300); // slot2
-	showObject(fruit[num2]);
+	locateObject(fruitB[slotB()], scene1, xLocate[1], 300); // B 슬롯에 무작위 과일 이미지를 출력한다 
+	showObject(fruitB[num2]);
 
-	locateObject(fruit[slot3()], scene1, xLocate[2], 300); // slot3
-	showObject(fruit[num3]);
+	locateObject(fruitC[slotC()], scene1, xLocate[2], 300); // C 슬롯에 무작위 과일 이미지를 출력한다
+	showObject(fruitC[num3]);
 
 	}
+ 
 
 void keyboardCallback(KeyCode code, KeyState state)
 {
-	if (code == 84 && state == KeyState::KEYBOARD_PRESSED) {		//up 버튼 클릭시
+
+	if (code == 84 && state == KeyState::KEYBOARD_PRESSED) { // up 버튼을 누를 때 코인 효과음이 나온다.
+		playSound(coin);
+
+	}
+
+	if (code == 84 && state == KeyState::KEYBOARD_RELEASED) { //up 버튼을 땔 때 룰렛이 작동한다.
 		Roulette();
-		if ((num1 + 3) == num2 && num2 == (num3 - 3) && num2 == 3) { // 사과 세개가 나올 경우
+		if (num1 == num2 && num2 == num3 && num3 == APPLE) { // 사과 세개가 나올 경우
 			showMessage("+5원");
+			playSound(win);
 		}
 
-		else if ((num1 + 3) == num2 && num2 == (num3 - 3) && num2 == 4 ) { // 포도 세개가 나올 경우
+		else if (num1 == num2 && num2 == num3 && num3 == GRAPE ) { // 포도 세개가 나올 경우
 			showMessage("+10원");
+			playSound(win);
 		}
 
-		else if ((num1 + 3) == num2 && num2 == (num3 - 3) && num2 == 5) { // 오렌지 세개가 나올 경우
+		else if (num1 == num2 && num2 == num3 && num3 == ORANGE) { // 오렌지 세개가 나올 경우
 			showMessage("+20원");
+			playSound(win);
 		}
 
 		else { 
@@ -119,8 +139,9 @@ void keyboardCallback(KeyCode code, KeyState state)
 		
 }
 
-int main() {
 
+int main() {
+	
 	setKeyboardCallback(keyboardCallback);
 	setTimerCallback(timerCallback);
 	srand((unsigned int)time(NULL));
@@ -129,8 +150,6 @@ int main() {
 	TimerID timer = createTimer(ANIMATION_TIME);
 	startTimer(timer);
 
-
 	startGame(scene1);
-
 
 }
