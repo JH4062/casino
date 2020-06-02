5c7ed26f8c353d982b153a5cc9fd5c8fbeafc5d1
박장현 <p5725@cau.ac.kr>
2020-06-02 20:19:56 +0900

    배경 이미지 추가함. 물음표를 이용해서 스핀 도는 애니메이션 추가함. 머신 이미지 교체함.

--- casino.cpp
+++ casino.cpp
@@ -1,6 +1,5 @@
 ﻿/* ======================================================================
 카지노 슬롯머신 
-6월 1일 기준 :: up 버튼을 누르면 슬롯머신이 동작하고 화면에 과일들이 나타난다. 과일 세개가 일치하면 돈을 획득한다.
 
 가격표
 게임 플레이 비용 -1원
@@ -11,9 +10,8 @@
 향후 필요한 것: 
 1. 키보드 입력을 스페이스로 교체
 2. money를 모든 씬과 연동해야함
-3. 이미지 필요: 슬롯 머신 뒷 배경/ 가격표
-4. 배경음악이 끝날때 callback으로 다시 실행시키고 싶은데 방법을 모르겠다
-5. 과일별 출현 확률 수정
+//3. 이미지 필요: 슬롯 머신 옆에 가격표
+5. 과일별 출현 확률 수정? 이를테면 사과>포도>오렌지 순으로 출현하게
 =========================================================================*/
 
 													
@@ -26,23 +24,30 @@
 #define GRAPE 1 
 #define ORANGE 2
 
-#define ANIMATION_TIME		2
+const Second animationTime = 0.05f; // 물음표가 화면에 출력되는 시간 간격
 
 ObjectID fruitA[3], fruitB[3], fruitC[3];
 int num1, num2, num3;
 
 SceneID scene1;
-ObjectID machine;
+ObjectID questionMark, machine;
 
 TimerID timer;
 
 SoundID bgm, coin, win;
 
+int spin = 0;
+
+bool game = 0; // 0은 스핀이 멈춘 상태, 1은 스핀이 돌아가고 있는 상태 
+
+int xLocate[3] = { 430, 580, 730 }; // 카지노 A B C 슬롯의 x좌표값
+
 void Gameinit() { 
 
-	scene1 = createScene("바탕화면", "고쳐야 됨!.png"); // 카지노 바탕화면 필요할듯
+	scene1 = createScene("카지노", "background.png"); 
 	machine = createObject("machine.png");
-	locateObject(machine, scene1, 350, 250);
+	locateObject(machine, scene1, 260, 25);
+	scaleObject(machine, 2.5f);
 	showObject(machine);
 
 	//슬롯머신에 사용되는 과일들 생성 후 배열에 넣어 사용
@@ -59,18 +64,17 @@ void Gameinit() {
 	bgm = createSound("casinoBackground.wav");
 	playSound(bgm);
 
-	coin = createSound("coinEffect.wav");
+	coin = createSound("coinEffect.wav"); // 동전 투입 효과음
 
-	win = createSound("win.mp3");
+	questionMark = createObject("questionMark.png"); 
+
+	win = createSound("win.mp3"); // 과일 세개가 일치할 때의 효과음
+
+	timer = createTimer(600.0f);
+	
 }
 
-void timerCallback(TimerID timer)
-{
-	setTimer(timer, ANIMATION_TIME);
-	startTimer(timer);
-}
-
-int slotA() {  //slotB에 무작위 과일에 해당하는 난수를 생성한다
+int slotA() {  //slotA에 무작위 과일에 해당하는 난수를 생성한다
 	num1 = rand() % 3;
 	return num1;
 }
@@ -80,63 +84,111 @@ int slotB() { //slotB에 무작위 과일에 해당하는 난수를 생성한다
 	return num2;
 }
 
-int slotC() { //slotB에 무작위 과일에 해당하는 난수를 생성한다
+int slotC() { //slotC에 무작위 과일에 해당하는 난수를 생성한다
 	num3 = rand() % 3;
 	return num3;
 }
 
+
+
 void Roulette() {
-	for (int i = 0; i < 3; i++) { // 화면에 보이는 카지노 슬롯 초기화
-		hideObject(fruitA[i]);
-		hideObject(fruitB[i]);
-		hideObject(fruitC[i]);
-	}
 
-	int xLocate[3] = { 380, 540, 700 };
+	game = 0; // 스핀이 멈췄음
 
-	locateObject(fruitA[slotA()], scene1, xLocate[0], 300); // A 슬롯에 무작위 과일 이미지를 화면에 출력한다
-	showObject(fruitA[num1]);
-	
-	locateObject(fruitB[slotB()], scene1, xLocate[1], 300); // B 슬롯에 무작위 과일 이미지를 출력한다 
-	showObject(fruitB[num2]);
-
-	locateObject(fruitC[slotC()], scene1, xLocate[2], 300); // C 슬롯에 무작위 과일 이미지를 출력한다
+	locateObject(fruitA[slotA()], scene1, xLocate[0], 310); // 난수를 이어 받아 A 슬롯에 과일 이미지를 출력한다
+	showObject(fruitA[num1]);						   
+													  
+	locateObject(fruitB[slotB()], scene1, xLocate[1], 310); // 난수를 이어 받아 B 슬롯에 과일 이미지를 출력한다 
+	showObject(fruitB[num2]);						  
+													  
+	locateObject(fruitC[slotC()], scene1, xLocate[2], 310); // 난수를 이어 받아  C 슬롯에 과일 이미지를 출력한다
 	showObject(fruitC[num3]);
 
+	if (num1 == num2 && num2 == num3 && num3 == APPLE) { // 사과 세개가 나올 경우 효과음 재생
+		showMessage("+5원");
+		playSound(win);
 	}
- 
+
+	else if (num1 == num2 && num2 == num3 && num3 == GRAPE) { // 포도 세개가 나올 경우 효과음 재생
+		showMessage("+10원");
+		playSound(win);
+	}
+
+	else if (num1 == num2 && num2 == num3 && num3 == ORANGE) { // 오렌지 세개가 나올 경우 효과음 재생
+		showMessage("+20원");
+		playSound(win);
+	}
+
+	else { // 게임 비용
+		showMessage("-1원");
+	}
+
+}
+	 
+void Spin() { // 물음표가 슬롯 A B C에 시간 간격을 두고 차례대로 생성됨 (룰렛 스핀)
+
+	game = 1; // 스핀이 돌아가는 중
+
+		for (int i = 0; i < 3; i++) { // 화면에 보이는 카지노 슬롯 초기화
+			hideObject(fruitA[i]);
+			hideObject(fruitB[i]);
+			hideObject(fruitC[i]);
+		}
+
+		spin = spin + 30; // 물음표 애니메이션 시간 간격 조정
+
+		//애니메이션
+		if (spin < 100) {
+			hideObject(questionMark);
+			showObject(questionMark);
+			locateObject(questionMark, scene1, xLocate[0], 300);
+			setTimer(timer, animationTime);
+			startTimer(timer);
+
+
+		}
+		else if (spin > 100 && spin < 200) {
+			hideObject(questionMark);
+			locateObject(questionMark, scene1, xLocate[1], 300);
+			showObject(questionMark);
+			setTimer(timer, animationTime);
+			startTimer(timer);
+		}
+
+		else if (spin > 200 && spin <= 300) {
+			hideObject(questionMark);
+			locateObject(questionMark, scene1, xLocate[2], 300);
+			showObject(questionMark);
+			setTimer(timer, animationTime);
+			startTimer(timer);
+		}
+
+		else {
+			hideObject(questionMark);
+			Roulette(); // 슬롯 C에 물음표가 출력 된 후에 무작위 과일들이 화면에 나타남
+		}
+}
+	
+
+void timerCallback(TimerID timer)
+{
+	if (timer == timer) { 
+		Spin();
+	}
+
+}
+	
 
 void keyboardCallback(KeyCode code, KeyState state)
 {
 
-	if (code == 84 && state == KeyState::KEYBOARD_PRESSED) { // up 버튼을 누를 때 코인 효과음이 나온다.
-		playSound(coin);
-
-	}
-
-	if (code == 84 && state == KeyState::KEYBOARD_RELEASED) { //up 버튼을 땔 때 룰렛이 작동한다.
-		Roulette();
-		if (num1 == num2 && num2 == num3 && num3 == APPLE) { // 사과 세개가 나올 경우
-			showMessage("+5원");
-			playSound(win);
-		}
-
-		else if (num1 == num2 && num2 == num3 && num3 == GRAPE ) { // 포도 세개가 나올 경우
-			showMessage("+10원");
-			playSound(win);
-		}
-
-		else if (num1 == num2 && num2 == num3 && num3 == ORANGE) { // 오렌지 세개가 나올 경우
-			showMessage("+20원");
-			playSound(win);
-		}
-
-		else { 
-			showMessage("-1원"); 
-		}
-
-	}
+	if (game == 0 && code == 84 && state == KeyState::KEYBOARD_PRESSED) { // 스핀이 멈췄을때 up 버튼을 누르면 룰렛이 스핀한다.
 		
+		spin = 0; 
+		Spin();
+		playSound(coin);
+	}
+	
 }
 
 
@@ -147,9 +199,6 @@ int main() {
 	srand((unsigned int)time(NULL));
 	Gameinit();
 	
-	TimerID timer = createTimer(ANIMATION_TIME);
-	startTimer(timer);
-
 	startGame(scene1);
 
 }
